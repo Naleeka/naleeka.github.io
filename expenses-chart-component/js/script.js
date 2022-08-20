f17b2dab74fb95ec3300fd3bf245e89de3b5b146
Naleeka <kumarasinghenaleeka@gmail.com>
2022-08-21 00:19:33 +0530

    done for mobile version

---
+++ expenses-chart-component/js/script.js
@@ -0,0 +1,75 @@
+// const chartArray = fetch("data.json")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     let list = JSON.stringify(data);
+//     console.log(list[0]);
+//   });
+
+const chartArray = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+var allAmounts = [];
+
+const chart = document.querySelector(".bars");
+
+window.addEventListener("load", () => {
+  chart.classList.add("loaded");
+  const maxA = checkMaxAmount();
+  for (let i = 0; i < chartArray.length; i++) {
+    const day = chartArray[i].day;
+    const amount = chartArray[i].amount;
+    if (amount === maxA) {
+      chart.innerHTML += `
+    <div class="${day}">
+    <span class="tooltip">$${amount}</span>
+      <span style="height: calc(${amount}px * 2.8); background:hsl(186, 34%, 60%) ; transition: height 0.3s ease;" class="bar bar-${i}"></span>
+      <span class="day">${day}</span>
+    </div>
+    `;
+    } else {
+      chart.innerHTML += `
+      <div class="${day}">
+      <span class="tooltip">$${amount}</span>
+        <span style="height: calc(${amount}px * 2.8); transition: height 0.3s ease;" class="bar bar-${i}"></span>
+        <span class="day">${day}</span>
+      </div>
+      `;
+    }
+  }
+});
+function checkMaxAmount() {
+  for (let i = 0; i < chartArray.length; i++) {
+    const amount = chartArray[i].amount;
+    allAmounts.push(amount);
+  }
+  return Math.max(...allAmounts);
+}
