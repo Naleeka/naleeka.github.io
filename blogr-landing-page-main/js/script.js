1f255daf9aceed228fb3267f28084f9b61ffadbb
Naleeka <kumarasinghenaleeka@gmail.com>
2022-08-25 17:39:00 +0530

    mobile version done

---
+++ blogr-landing-page-main/js/script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector(".header"),
+  main = document.querySelector("main"),
+  closeBtn = document.querySelector(".close-btn"),
+  menuBtn = document.querySelector(".menu-btn"),
+  checkBoxes = document.querySelectorAll(
+    ".nav > ul > li > input[type='checkbox']"
+  );
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticks", window.scrollY > 1);
+});
+
+menuBtn.addEventListener("click", () => {
+  main.classList.add("active");
+  if (!header.classList.contains("sticks")) {
+    header.classList.add("sticks");
+  }
+});
+closeBtn.addEventListener("click", () => {
+  main.classList.remove("active");
+  header.classList.toggle("sticks", window.scrollY > 1);
+});
