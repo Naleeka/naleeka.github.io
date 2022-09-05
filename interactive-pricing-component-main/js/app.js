4bb49aad9c2e9b837d1700b77d76ad02cd57191b
Naleeka <107068435+Naleeka@users.noreply.github.com>
2022-09-05 21:52:10 +0530

    finished

--- interactive-pricing-component-main/js/app.js
+++ interactive-pricing-component-main/js/app.js
@@ -1,15 +1,17 @@
 const checkBox = document.querySelector("#bill"),
   packageName = document.getElementById("packName"),
+  monthEl = document.querySelector(".month"),
+  yearEl = document.querySelector(".year"),
   packagePrice = document.querySelector("#packPrice span"),
   slider = document.querySelector("input[type='range']");
 
 checkBox.addEventListener("change", () => {
   if (checkBox.checked) {
-    checkBox.parentElement.querySelector(".year").classList.add("active");
-    checkBox.parentElement.querySelector(".month").classList.remove("active");
+    yearEl.classList.add("active");
+    monthEl.classList.remove("active");
   } else {
-    checkBox.parentElement.querySelector(".year").classList.remove("active");
-    checkBox.parentElement.querySelector(".month").classList.add("active");
+    yearEl.classList.remove("active");
+    monthEl.classList.add("active");
   }
   changeVals(slider.value, checkBox.checked, list);
 });
